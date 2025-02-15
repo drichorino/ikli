94ae7666ca113b7af346cfd79109b3a00594f24d
Aldrich <aldrich.escudero@gmail.com>
2025-02-16 00:11:18 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URL: str = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-    MONGO_DB: str = os.getenv("MONGO_DB", "url_shortener")
+    MONGO_DB: str = os.getenv("MONGO_DB", "link_apex_db")
 
     class Config:
         env_file = ".env"
